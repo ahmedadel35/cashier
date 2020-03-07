31005cd89b5bc07b52ec942ce67614e6b7376498
ahmed adel <abo3adel35@gmail.com>
2020-03-07 16:35:06 +0200

    load data with proper headers

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,17 +1,50 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
   public serverPath = 'http://cx.test/';
+  private Headers: HttpHeaders;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+      const h = new HttpHeaders();
+      h.append('Content-Type', 'application/x-www-form-urlencoded');
+      h.append('Response-Type', 'application/json; charset=utf-8');
 
-  get() {
-    this.http.get(this.serverPath).subscribe(r => {
-      console.log(r);
-    }, err => console.log(err));
+  }
+
+   get(url: string): Observable<{} | any> {
+    return this.http.get(this.serverPath + url, {headers: this.Headers});
+  }
+
+  post(url: string, data: any) {
+    return this.http.post(this.serverPath + url, data, {headers: this.Headers});
+  }
+
+  put(url: string, data: any) {
+    return this.http.put(this.serverPath + url, data, {headers: this.Headers});
+  }
+
+  async delete(url: string) {
+    const d = await this.http.delete(this.serverPath + url, {headers: this.Headers});
+
+    console.log(d);
+
+    d.subscribe(
+      (x: { deleted: boolean }) => {
+        if (x && x.deleted) {
+          return true;
+        } else {
+          return false;
+        }
+      },
+      err => {
+        console.log(err);
+        return false;
+      }
+    );
   }
 }
