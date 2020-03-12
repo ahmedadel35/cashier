adc392e681c88685071e71fd85dea0dcf2acdeb3
ahmed adel <abo3adel35@gmail.com>
2020-03-12 22:45:05 +0200

    remove some imports

--- src/app/angular-material.module.ts
+++ src/app/angular-material.module.ts
@@ -6,24 +6,25 @@ import { LayoutModule } from '@angular/cdk/layout';
 // Form Controls
 //
 
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { MatListModule } from '@angular/material/list';
+// import { MatAutocompleteModule } from '@angular/material/autocomplete';
+// import { MatListModule } from '@angular/material/list';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 // import { MatNativeDateModule } from '@angular/material/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
-import { MatRadioModule } from '@angular/material/radio';
+// import { MatRadioModule } from '@angular/material/radio';
 import { MatSelectModule } from '@angular/material/select';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatSidenavModule } from '@angular/material/sidenav';
+// import { MatGridListModule } from '@angular/material/grid-list';
+// import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatTableModule } from '@angular/material/table';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatDialogModule } from '@angular/material/dialog';
+// import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 // import { MatSliderModule } from '@angular/material/slider';
 // import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
@@ -56,25 +57,26 @@ export const MY_FORMATS = {
 const modules: any[] = [
     LayoutModule,
 
-    MatAutocompleteModule,
+    // MatAutocompleteModule,
     MatCheckboxModule,
     MatDatepickerModule,
     MatFormFieldModule,
     MatInputModule,
-    MatRadioModule,
+    // MatRadioModule,
     MatSelectModule,
-    MatGridListModule,
+    // MatGridListModule,
     // MatSliderModule,
     // MatSlideToggleModule,
-    MatListModule,
+    // MatListModule,
     MatButtonModule,
-    MatSidenavModule,
+    // MatSidenavModule,
     MatToolbarModule,
     MatIconModule,
     MatButtonToggleModule,
     MatTableModule,
     MatMenuModule,
     MatDialogModule,
+    // MatProgressSpinnerModule,
     // MatNativeDateModule,
     // MatMomentDateModule,
 ];
