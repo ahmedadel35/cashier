35cbf9c15fb26c9d9262c59de9b46d2f9eb3bd33
ahmed adel <abo3adel35@gmail.com>
2020-03-12 23:00:41 +0200

    handle adding new type or brand

--- src/app/components/addnew/addnew.component.ts
+++ src/app/components/addnew/addnew.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit, Inject, ViewChild } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Brand } from '../../interfaces/brand';
 import { Type } from '../../interfaces/type';
 import { ApiService } from '../../services/api.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
     selector: 'app-addnew',
@@ -10,11 +11,14 @@ import { ApiService } from '../../services/api.service';
     styleUrls: ['./addnew.component.scss']
 })
 export class AddnewComponent implements OnInit {
+    public result: any;
     public isBrand = false;
     public types: Type[] = [];
     public typeId: number;
     public name: string;
     public isLoading = false;
+
+    @ViewChild('addTypeOrBrand') form: NgForm;
 
     constructor(
         public dialogRef: MatDialogRef<AddnewComponent>,
@@ -30,7 +34,12 @@ export class AddnewComponent implements OnInit {
         // console.log(this.data);
     }
 
-    save(f) {
+    save(f): void {
+        if (this.isLoading || !f.name || !f.name.length) {
+            return;
+        }
+
+        this.isLoading = true;
         const obj = {
             name: f.name,
             typeId: null
@@ -38,16 +47,40 @@ export class AddnewComponent implements OnInit {
 
         if (this.isBrand) {
             obj.typeId = f.typeId;
+
+            this.api.post(`type/${f.typeId}`, obj).subscribe(
+                r => {
+                    // console.log(r);
+                    this.isLoading = false;
+                    this.result = r;
+                    this.typeId = null;
+                    this.name = '';
+                },
+                err => {
+                    console.log(err);
+                    this.isLoading = false;
+                }
+            );
         }
 
-        console.log(obj);
+        // console.log(obj);
 
-        this.api.post('type', obj).subscribe(r => {
-            console.log(r);
-        }, err => console.log(err));
+        this.api.post('type', obj).subscribe(
+            r => {
+                this.isLoading = false;
+                // console.log(r);
+                this.result = r;
+                this.name = '';
+                this.close();
+            },
+            err => {
+                console.log(err);
+                this.isLoading = false;
+            }
+        );
     }
 
     close() {
-        this.dialogRef.close();
+        this.dialogRef.close(this.result);
     }
 }
