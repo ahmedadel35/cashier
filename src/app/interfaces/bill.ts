55e3139f7fa8092311067ea8a77b6a5e76f1825e
ahmed adel <abo3adel35@gmail.com>
2020-03-20 22:13:10 +0200

    fix dublicate identefier

--- src/app/interfaces/bill.ts
+++ src/app/interfaces/bill.ts
@@ -10,7 +10,6 @@ export interface Bill {
     state?: string | 'hot' | 'cold';
     date?: string;
     created_at?: string;
-    date?: string;
     updated_at?: string;
     type?: Type;
     brand?: Brand;
